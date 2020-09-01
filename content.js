5ddfbd03b6dee4a9c3775fa374234dc3c09a6378
Jay Chen <walkccray@gmail.com>
2020-09-01 18:46:24 -0400

    Add closing Console panel functionality

--- content.js
+++ content.js
@@ -4,4 +4,10 @@ document.addEventListener('keydown', function onEvent(e) {
   if (e.ctrlKey && e.keyCode === 13) {
     document.querySelector('.submit__2ISl').click();
   }
+
+  // "ctrl+`": close the 'x' and Console panel like a pro!
+  if (e.ctrlKey && e.keyCode === 192) {
+    const toClose = document.querySelector('.icon-wrapper__2q8n');
+    if (toClose !== null) toClose.click();
+  }
 });
